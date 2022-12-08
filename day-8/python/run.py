e79066d2466498eed9ceba65effa3b7dd0f4491b
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2022-12-08 17:42:24 +0000

    Merge solutions for part 1 and 2 of day 8.

--- day-8/python/run.py
+++ day-8/python/run.py
@@ -1,6 +1,5 @@
 import numpy as np
 from pathlib import Path
-from typing import List
 
 
 EXAMPLE_DATA_PATH = Path.cwd().parent / "data" / "example.dat"
@@ -9,90 +8,64 @@ TEST_DATA_PATH = Path.cwd().parent / "data" / "test.dat"
 
 def solve_pt1(x: np.ndarray) -> int:
     (m, n) = x.shape
-    outer_trees = 2 * (m + n) - 4  # outer edges
-    visible_tree_coords = []
-    for i in range(1, m - 1):
-        for j in range(1, n - 1):
-            if x[i, j] > np.max(x[i, :j]):  # trees to the left
-                visible_tree_coords.append((i, j))
-            if x[i, j] == 9:  # nothing can be taller
-                break
-        for j in reversed(range(1, n - 1)):
-            if x[i, j] > np.max(x[i, j+1:]):  # trees to the right
-                visible_tree_coords.append((i, j))
-            if x[i, j] == 9:  # nothing can be taller
-                break
-    for j in range(1, n - 1):
-        for i in range(1, m - 1):
-            if x[i, j] > np.max(x[:i, j]):  # trees above
-                visible_tree_coords.append((i, j))
-            if x[i, j] == 9:  # nothing can be taller
-                break
-    for j in range(1, n - 1):
-        for i in reversed(range(1, m - 1)):
-            if x[i, j] > np.max(x[i+1:, j]):  # trees below
-                visible_tree_coords.append((i, j))
-            if x[i, j] == 9:  # nothing can be taller
-                break
-    unique_visible_tree_coords = list(set(visible_tree_coords))
-    return outer_trees + len(unique_visible_tree_coords)
-
-
-def solve_pt2(x: np.ndarray) -> int:
-    (m, n) = x.shape
-    scenic_scores = []
+    visible_trees = 2 * (m + n) - 4
     for i in range(1, m - 1):
         for j in range(1, n - 1):
             tree = x[i, j]
-            trees_left = 0
-            trees_right = 0
-            trees_up = 0
-            trees_down = 0
-            for t in x[:i, j]:
-                trees_left += 1
-                if t >= tree:
-                    break
-            for t in x[i+1:, j]:
-                trees_right += 1
-                if t >= tree:
-                    break
-            for t in x[i, :j]:
-                trees_up += 1
-                if t >= tree:
-                    break
-            for t in x[i, j+1:]:
-                trees_down += 1
-                if t >= tree:
-                    break
-            print(f"({i}, {j}, {tree}) = [{trees_left}, {trees_right}, {trees_up}, {trees_down}]")
-            scenic_scores += [trees_left * trees_right * trees_up * trees_down]
-    return max(scenic_scores)
+            (left, right, up, down) = (x[i, :j], x[i, j + 1:], x[:i, j], x[i + 1:, j])
+            if any(np.all(tree > other) for other in (left[::-1], right, up[::-1], down)):
+                visible_trees += 1
+    return visible_trees
+
+
+def solve_pt2(x: np.ndarray) -> int:
+    def view_dist(tree, others):
+        return next((i + 1 for (i, t) in enumerate(others) if t >= tree), len(others))
+    (m, n) = x.shape
+    max_scenic_score = 0
+    for i in range(1, m - 1):
+        for j in range(1, n - 1):
+            tree = x[i, j]
+            (left, right, up, down) = (x[i, :j], x[i, j + 1:], x[:i, j], x[i + 1:, j])
+            n_left = view_dist(tree, left[::-1])
+            n_right = view_dist(tree, right)
+            n_up = view_dist(tree, up[::-1])
+            n_down = view_dist(tree, down)
+            scenic_score = n_left * n_right * n_up * n_down
+            if scenic_score > max_scenic_score:
+                max_scenic_score = scenic_score
+    return max_scenic_score
 
 
 def load(fpath: str) -> np.ndarray:
     text = None
     with open(fpath, "r") as f:
         text = f.read().split("\n")
-    data = np.array([[int(c) for c in line] for line in text])
-    return data
+    return np.array([[int(c) for c in line] for line in text])
 
 
 def main() -> int:
     data1 = load(EXAMPLE_DATA_PATH)
     data2 = load(TEST_DATA_PATH)
 
-    example_answer1 = 21
-    example_answer2 = 8
+    example_solution1 = 21
+    example_solution2 = 8
+    test_solution1 = 1823
+    test_solution2 = 211680
 
-    answer1 = solve_pt1(data1)
-    print(f"[EXAMPLE] Answer to Part 1: {answer1}")
-    assert answer1 == example_answer1
-    print(f"[TEST] Answer to Part 1: {solve_pt1(data2)}")
+    example_answer1 = solve_pt1(data1)
+    test_answer1 = solve_pt1(data2)
+    print(f"[EXAMPLE] Answer to Part 1: {example_answer1}")
+    print(f"[TEST] Answer to Part 1: {test_answer1}")
+    assert example_answer1 == example_solution1
+    assert test_answer1 == test_solution1
 
-    answer2 = solve_pt2(data1)
-    print(f"[EXAMPLE] Answer to Part 2: {answer2}")
-    assert answer2 == example_answer2
-    # print(f"[TEST] Answer to Part 2: {solve_pt2(data2)}")
+    example_answer2 = solve_pt2(data1)
+    test_answer2 = solve_pt2(data2)
+    print(f"[EXAMPLE] Answer to Part 2: {example_answer2}")
+    print(f"[TEST] Answer to Part 2: {test_answer2}")
+    assert example_answer2 == example_solution2
+    assert test_answer2 == test_solution2
 
 
 if __name__ == "__main__":
